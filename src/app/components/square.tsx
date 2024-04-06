eff45e916a7abc53325e236907afc503d5978230
riadfrancoq <campusricardo2023@gmail.com>
2024-04-06 02:33:09 -0500

    feat: :sparkles: Finished sudoku : )

--- src/app/components/square.tsx
+++ src/app/components/square.tsx
@@ -8,10 +8,36 @@ export default function Square({
     const [values, setValues] = useState(Array(element.length).fill(""));
 
     const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>, i: number) => {
+        let pointer: number = index;
+        let verPointer: number = index;
+
+        if (verPointer === 0 || verPointer === 1 || verPointer === 2) {
+            verPointer = index;
+        } else if (verPointer === 3 || verPointer === 6 ) {
+            verPointer = 0;
+        } else if (verPointer === 4 || verPointer === 7) {
+            verPointer = 1;
+        }  else if (verPointer === 5 || verPointer === 8) {
+            verPointer = 2;
+        }
+
+        if (pointer  % 3 === 0 ) {
+
+            pointer = index;
+        } else if (pointer === 8) {
+                pointer = 6;
+        } else {
+            pointer -= pointer % 3;
+        }
+        
         let exists: boolean = false;
         const userInput: number = parseInt(e.target.value);
+        if (isNaN(userInput)) {
+            return;
+        }
         for ( let a = 0; a < matrix.length; a++) {
-            if (a = index) {
+
+            if (a === index) {
                 for ( let j = 0; j < 3; j++) {
 
                     if (userInput === matrix[a][j][0] || userInput === matrix[a][j][1] || userInput === matrix[a][j][2]) {
@@ -20,11 +46,26 @@ export default function Square({
                  }
 
                 }
+
+                for (let b = 0; b < 3; b++) {
+
+                   for ( let k = 0; k < 3; k++) {
+                    if ( userInput === matrix[pointer][row][k] || 
+                        userInput === matrix[verPointer][k][i]) {
+                    console.log(verPointer, row, index, pointer, i, k)
+
+                        exists = true;
+                        
+                        break;
+                    }
+                   }
+                   pointer ++;
+                   verPointer += 3;
+                }
         }
         }
         if (exists === false) { 
         matrix[index][row][i] = userInput;
-            console.log(matrix);
             const newValues = [...values];
             newValues[i] = userInput;
             setValues(newValues);
